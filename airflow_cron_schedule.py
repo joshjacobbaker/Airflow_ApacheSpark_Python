d12058091b9a632ea6b594f3c96c339532cf8e50
Josh <josh.jacob.baker@gmail.com>
2021-02-05 22:32:12 -0800

    adding a Bash Operator

--- airflow_cron_schedule.py
+++ airflow_cron_schedule.py
@@ -22,3 +22,15 @@ tasks_with_tomato_sauce_parent >> bake_pizza
 
 # Specify direction using verbose method
 bake_pizza.set_upstream(prepare_oven)
+
+config = os.path.join(os.environ["AIRFLOW_HOME"], 
+                      "scripts",
+                      "configs", 
+                      "data_lake.conf")
+
+ingest = BashOperator(
+  # Assign a descriptive id
+  task_id="ingest_data", 
+  # Complete the ingestion pipeline
+  bash_command='tap-marketing-api | target-csv --config %s' % config,
+  dag=dag)
